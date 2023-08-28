41b1254de6f9de02015720abb32ea24612044f33
Maxim Kozlov <sebekerka@gmail.com>
2023-08-28 19:02:31 +0600

    added passable example to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-//! **parakeet** provides an easy to use wrapper around [headless_chrome](https://docs.rs/headless_chrome)
+//! **parakeet** implements an easy to use wrapper around [headless_chrome](https://docs.rs/headless_chrome)
 //! that provides an asynchronous function for PDF rendering. This is useful for rendering PDFs in a web server,
 //! as launching is slow, therefore should be done once. It controls many tabs at once, giving you the ability
 //! to render multiple PDFs at once.
+//!
+//! # Example
+//! ```rust
+//! use std::sync::Arc;
+//! use parakeet_pdf::{
+//!     page_props::PagePropertiesBuilder,
+//!     rendering_engine::RenderingEngine,
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     const TAB_COUNT: usize = 5;
+//!     let engine = Arc::new(RenderingEngine::new(TAB_COUNT).unwrap());
+//!
+//!     let html = format!("Hello, html!");
+//!     let page_props = PagePropertiesBuilder::from_size(10.0, 10.0).build();   
+//!     let _pdf = engine.render_html(&html, page_props).await.unwrap();
+//! }
+//! ```
 //!
 pub mod browser;
 pub mod page_props;
